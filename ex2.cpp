2796af54e8515b7aa75574511a87517c1bbf2c53
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2020-05-25 22:40:59 -0300

    small fixes

--- ex2.cpp
+++ ex2.cpp
@@ -8,7 +8,7 @@ int main (){
     int val;
 
     std::ifstream ifs;
-    ifs.open("input.txt");
+    ifs.open("input_ex2.txt");
 
     while (ifs >> val)
         numbers.push_back(val);
@@ -16,7 +16,7 @@ int main (){
     ifs.close();
 
     std::ofstream ofs;
-    ofs.open ("invertido.txt");
+    ofs.open ("output_ex2.txt");
 
     for (int i{0}; i < (int) numbers.size(); i++){
         ofs << numbers.at(numbers.size() - 1 - i) << "\n";
